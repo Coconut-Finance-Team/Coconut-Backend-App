ceb883a42e1f4749436381d6b5da3008a4b58dcb
최수연 <tndus5383@gmail.com>
2024-12-05 19:24:39 +0900

    Update ElasticSearchConfig.java

--- src/main/java/com/coconut/stock_app/config/ElasticSearchConfig.java
+++ src/main/java/com/coconut/stock_app/config/ElasticSearchConfig.java
@@ -6,20 +6,17 @@ import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-import java.util.List;
-
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.coconut.stock_app.repository.elasticsearch")
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
     @Value("${spring.elasticsearch.uris}")
-    private List<String> elasticsearchUrls; // 배열 형태로 값 주입
+    private String elasticsearchUrl;
 
     @Override
     public ClientConfiguration clientConfiguration() {
-        // 리스트를 배열로 변환하여 전달
         return ClientConfiguration.builder()
-                .connectedTo(elasticsearchUrls.toArray(new String[1])) // 배열로 전달
+                .connectedTo(elasticsearchUrl.split(","))
                 .build();
     }
 }
